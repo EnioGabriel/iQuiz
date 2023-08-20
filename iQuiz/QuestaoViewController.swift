1b3cf4f03764cb1180b71e96fde44a61afb40a61
Enio Gabriel <gabrielferreira.sc@gmail.com>
2023-08-20 19:31:34 -0300

    feat: add navegação entre telas

---
+++ iQuiz/QuestaoViewController.swift
@@ -0,0 +1,15 @@
+//
+//  QuestaoViewController.swift
+//  iQuiz
+//
+//  Created by Enio  Gabriel Ferreira Silva on 20/08/23.
+//
+
+import UIKit
+
+class QuestaoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
